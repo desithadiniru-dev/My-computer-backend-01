f6a04f6a4dd5d0b6bd952c856edf21cc7877c7d3
Desitha Somanayake <desithadiniru@gmail.com>
2026-07-03 14:52:45 +0530

    Removing unnecessary files

--- routers/studentRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createStudent, getStudents } from "../controllers/studentController.js";
-
-const studentRouter = express.Router();
-
-studentRouter.post("/", createStudent);
-studentRouter.get("/", getStudents);
-
-export default studentRouter;
